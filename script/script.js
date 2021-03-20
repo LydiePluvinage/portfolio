43b2a3aa34bad1783f52d6b416b5f8cf5cb94dc4
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2021-03-20 21:52:56 +0100

    Optimisation du javascript

--- script/script.js
+++ script/script.js
@@ -13,53 +13,30 @@ document.addEventListener('scroll', () => {
     const skillList = document.querySelector('.skills__content');
     const isSkillVisible = top > skillList.offsetTop;
     if (isSkillVisible) {
-        const areas = document.getElementsByClassName('skills__area');
+        const areas = skillList.getElementsByClassName('skills__area');
         for (let i = 0; i < areas.length; i += 1) {
             const widthAnimation = areas[i].getAttribute('data-width');
             areas[i].animate({ width: widthAnimation }, { duration: 2000, fill: 'forwards' });
         }
     }
     // Summary animation
-    const summaryList = document.querySelector('.summary > .summary__content > ul');
+    const summaryList = document.getElementById('summaryList');
     const isSummaryVisible = top > summaryList.offsetTop;
 
     if (isSummaryVisible) {
-        if (!document.querySelector('.summary > .summary__content > ul > li .animate')) {
-            for (let i = 0; i < summaryList.children.length; i += 1) {
-                summaryList.children[i].classList.add('animate');
-            }
+        if (!summaryList.querySelector('.summary_item.animate')) {
+            summaryList
+                .querySelectorAll('.summary__item')
+                .forEach((item) => item.classList.add('animate'));
         }
     }
 });
 
-// APPLY COLORS IN PARAMETERS
+// SWITCH DARK/LIGHT MODE
 function switchColors() {
-    // Body
-    let element = document.body;
+    const element = document.body;
     element.classList.toggle('dark');
-
-    // First quote
-    element = document.querySelector('.quote > .quote__text.quote__style');
-    element.classList.toggle('dark');
-    element = element.querySelector('.quote__author');
-    element.classList.toggle('dark');
-
-    // Button in introduction section
-    element = document.querySelector('.introduction__button > .btn');
-    element.classList.toggle('dark');
-
-    // H3 titles
-    document.querySelectorAll('h3').forEach((icon) => {
-        icon.classList.toggle('dark');
-    });
-
-    // Summary icons
-    document.querySelectorAll('.summary__icon').forEach((icon) => {
-        icon.classList.toggle('dark');
-    });
 }
 
-// DEFINE COLORS DARK/LIGHT
-document.getElementById('selectorMode').addEventListener('change', () => {
-    switchColors();
-});
+// ACTIVATE SWITCH WHEN USER CHANGES MODE
+document.getElementById('selectorMode').addEventListener('change', switchColors);
